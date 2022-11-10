1742852c3740f5c8a73251ac6610c3318981e8bc
Datta0237 <dattapatil9764@gmail.com>
2022-11-10 21:50:33 +0530

    updated code

---
+++ cart.js
@@ -0,0 +1,164 @@
+let  products =[
+    {
+        id:1,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/md/D-PR00002153.jpg?ver=21.87',
+        name:'Peri Peri 10 Strips with Dynamite',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹639.05',
+        offerprice:'₹639.05',
+        serves:'Serves 2-3',
+        discripation:'10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle',
+        category:'chicken buckets'
+
+    },
+    {
+        id:2,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/md/D-PR00000892.jpg?ver=21.87',
+        name:'Bucket for Two',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹744.00',
+        offerprice:'₹7599.00',
+        serves:'Serves 2-3',
+        discripation:'Flat Rs. 115 off on combo of 2pc Hot & Crispy, 2 pc Smoky Red Chicken & Large Popcorn',
+        category:'chicken buckets'
+
+    },
+    {
+        id:3,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002154.jpg?ver=21.87',
+        name:'Peri Peri 10 Strips with Nashville',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹639.05',
+        offerprice:'₹639.05',
+        serves:'Serves 2-3',
+        discripation:'10 Spicy Peri Peri chicken strips & our Nashville Hot Sauce Bottle',
+        category:'chicken buckets'
+
+    },
+    {
+        id:4,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002155.jpg?ver=21.87',
+        name:'Peri Peri 10 Leg pc & 4 Dips',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹639.05',
+        offerprice:'₹639.05',
+        serves: 'Serves 3-4',
+        discripation:'10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle',
+        category:'chicken buckets'
+
+    },
+    {
+        id:5,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002156.jpg?ver=21.87',
+        name:'Peri Peri 5 Leg Pc Meal',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹628.57',
+        offerprice:'₹628.57',
+        serves: 'Serves 3-4',
+        discripation:'10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle',
+        category:'chicken buckets'
+    },
+    {
+        id:6,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002157.jpg?ver=21.87',
+        name:'Peri Peri 5 Leg Pc & 2 Dips',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹512.24',
+        offerprice:'₹512.24',
+        serves: 'Serves 2',
+        discripation:'5 Spicy Peri Peri Leg Pieces, paired with 2 delicous dips',
+        category:'chicken buckets'
+    },
+    //new launch
+    {
+        id:7,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002153.jpg?ver=21.87',
+        name:'Peri Peri 10 Strips with Dynamite',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹639.05',
+        offerprice:'₹639.05',
+        serves: 'Serves 2-3',
+        discripation:'10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle',
+        category:'new launch'
+    },
+    {
+        id:8,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002164.jpg?ver=21.87',
+        name:'Solo Feast',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹399.05',
+        offerprice:'₹399.05',
+        serves: 'Serves 2-3',
+        discripation:'Treat yourself with 1 Hot & Crispy, 2 Strips, 2 Wings, 1 Buttered Bun, 1 Dip & Pe₹399.05',
+        category:'new launch'
+    },
+    {
+        id:8,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002165.jpg?ver=21.87',
+        name:'Strips & Rice Combo',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹359.06',
+        offerprice:'₹359.06',
+        serves: 'Serves 1-2',
+        discripation:'4 strips, 1 Dips, flavorful Rice & Gravy at a deal price',
+        category:'new launch'
+    },
+    {
+        id:9,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002172.jpg?ver=21.87',
+        name:'Friendship Bucket with Dynamite Sauce',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹879.05',
+        offerprice:'₹879.05',
+        serves: 'Serves 3-4',
+        discripation:'3pc Hot & Crispy, 3 Wings, 3 Strips, Large Popcorn & our new Dynamite Mayo Bottle',
+        category:'new launch'
+    },
+    {
+        id:10,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002156.jpg?ver=21.87',
+        name:'Peri Peri 5 Leg Pc Meal',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹628.57',
+        offerprice:'₹628.57',
+        serves: 'Serves 3-4',
+        discripation:'5 Spicy Peri Peri Leg Pieces, 2 Dips, 1 Med Fries & a Pepsi PET',
+        category:'new launch'
+    },
+    {
+        id:11,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002123.jpg?ver=21.87',
+        name:'Mega Feast',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹399.04',
+        offerprice:'₹399.04',
+        serves: ' Serves 2-3',
+        discripation:'Feast up with 2 Hot & Crispy, 2 Smoky Red Chicken, 1 Bun, 1 Rice & gravy, Med Fries & Pepsi PET',
+        category:'new launch'
+    },
+    {
+        id:12,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002094.jpg?ver=21.87',
+        name:'6pc Hot & Crispy with Nashville Sauce',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹779.05',
+        offerprice:'₹779.05',
+        serves: ' Serves 3',
+        discripation:'6pc Hot & Crispy Chicken paired with our new Nashville Hot Pepper Sauce Bottle',
+        category:'new launch'
+    },
+    //biryani buckets
+    {
+        id:13,
+        Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002166.jpg?ver=21.87',
+        name:'Classic Biryani Combo',
+        logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
+        price:'₹740.00',
+        offerprice:'₹740.00',
+        serves: ' Serves 3',
+        discripation:'Large portions of Biryani rice with 2 pc Hot & Crispy, 2 Gravies, 4 Strips & Pepsi PET',
+        category:'biryani buckets'
+    },
+    
+
+]
