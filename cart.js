60412b087dc57c207cc0cc1fb1ddd6eb2831c3cf
Datta0237 <dattapatil9764@gmail.com>
2022-11-11 20:47:12 +0530

    added func

--- cart.js
+++ cart.js
@@ -4,7 +4,7 @@ let  products =[
         Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/md/D-PR00002153.jpg?ver=21.87',
         name:'Peri Peri 10 Strips with Dynamite',
         logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
-        price:'₹639.05',
+        price:'639.05',
         offerprice:'₹639.05',
         serves:'Serves 2-3',
         discripation:'10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle',
@@ -16,7 +16,7 @@ let  products =[
         Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/md/D-PR00000892.jpg?ver=21.87',
         name:'Bucket for Two',
         logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
-        price:'₹744.00',
+        price:'744.00',
         offerprice:'₹7599.00',
         serves:'Serves 2-3',
         discripation:'Flat Rs. 115 off on combo of 2pc Hot & Crispy, 2 pc Smoky Red Chicken & Large Popcorn',
@@ -28,7 +28,7 @@ let  products =[
         Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002154.jpg?ver=21.87',
         name:'Peri Peri 10 Strips with Nashville',
         logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
-        price:'₹639.05',
+        price:'639.05',
         offerprice:'₹639.05',
         serves:'Serves 2-3',
         discripation:'10 Spicy Peri Peri chicken strips & our Nashville Hot Sauce Bottle',
@@ -40,7 +40,7 @@ let  products =[
         Image:'https://orderserv-kfc-assets.yum.com/15895bb59f7b4bb588ee933f8cd5344a/images/items/xl/D-PR00002155.jpg?ver=21.87',
         name:'Peri Peri 10 Leg pc & 4 Dips',
         logo:'https://online.kfc.co.in/static/media/Non_veg_dot_Icon.d975d1f9.svg',
-        price:'₹639.05',
+        price:'639.05',
         offerprice:'₹639.05',
         serves: 'Serves 3-4',
         discripation:'10 Spicy Peri Peri chicken strips & our Dynamite Spicy Mayo Sauce Bottle',
@@ -161,4 +161,101 @@ let  products =[
     },
     
 
-]
+];
+
+console.log(products);  
+
+document.getElementById("startOrderButton").addEventListener("click",()=>{
+    startOrder();
+});
+
+let startOrder = ()=>{
+    renderDOM(products);
+    document.getElementById("cartlanding").innerHTML="";
+
+};
+
+let renderDOM = (Data) =>{
+    let count=1;
+    let container = document.getElementById("CartProducts");
+    container.innerHTML="";
+    Data.forEach(({id,Image,name,price},ele,index,)=>{
+        let div = document.createElement("div");
+        let imgdiv = document.createElement("div");
+        let namediv = document.createElement("div");
+        let pricediv = document.createElement("div");
+        pricediv.classList.add("PriceDiv");
+        div.classList.add("ProDiv");
+        let Img = document.createElement("img");
+        Img.src=Image;
+        let Name = document.createElement("p");
+        Name.innerText=name;
+        let Price = document.createElement("p");
+        Price.innerText=+price;
+        let proprice = +price;
+        console.log(proprice);
+        let Addcartbtn = document.createElement("h4");
+        Addcartbtn.innerText="Add TO Cart";
+        Addcartbtn.style.cursor="pointer";
+        Addcartbtn.style.textDecoration="underline";
+        Addcartbtn.addEventListener("click",function(){
+            addToCart(ele);
+        });
+        let btn = document.createElement("h4");
+        btn.innerText="Remove";
+        btn.style.cursor="pointer";
+        btn.style.textDecoration="underline";
+        btn.onclick=()=>{
+            RemoveCart(index);
+        }
+
+        let counter = document.createElement("h4");
+        counter.innerText=count;
+        
+
+        let minusbtn = document.createElement("img");
+        minusbtn.src="https://static.vecteezy.com/system/resources/previews/000/440/324/original/minus-vector-icon.jpg";
+        minusbtn.onclick=()=>{  
+            count--;
+            counter.innerText=count;
+            let res = count+proprice;
+            Price.innerText=res;
+            console.log(count);
+        }
+        let addbtn = document.createElement("img");
+        addbtn.src="https://library.kissclipart.com/20190320/gq/kissclipart-portable-network-graphics-clip-art-circle-computer-702c4539d6a09799.png";
+        addbtn.onclick=()=>{
+            count++;
+            counter.innerText=count;
+            Price.innerText=count+price;
+            console.log(count);
+        }
+        imgdiv.append(Img,Addcartbtn);
+        namediv.append(Name,btn);
+        pricediv.append(minusbtn,counter,addbtn,price);
+        div.append(imgdiv,namediv,pricediv);
+        container.append(div);
+    });
+};
+
+
+let cartData = JSON.parse(localStorage.getItem("cart")) || [];
+let RemoveCart = (index)=>{
+    cartData.splice(1,index);
+    localStorage.setItem("cart",JSON.stringify(cartData));
+}
+
+function addToCart(ele){
+    for(i=0;i<cartData.length;i++){
+      if(cartData[i].id===ele.id){
+        alert ("Prodct already exist in cart");
+        return;
+      }
+    }
+
+
+    cartData.push(ele)
+    localStorage.setItem("cart",JSON.stringify(cartData));
+    alert("Product Successfully added in cart");
+  };
+
